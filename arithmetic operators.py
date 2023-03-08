065ad98a5ec70c9551697c269a4dd1772c797955
AustinMusuya <126870037+AustinMusuya@users.noreply.github.com>
2023-03-08 14:03:27 +0300

    Add files via upload
    These are basics to guide all beginners who would like to learn Python fast! Assignments are included

---
+++ arithmetic operators.py
@@ -0,0 +1,18 @@
+#Arithmetic operators
+#use POMAS or BODMAS
+#Example
+a = 5
+b = 8
+c = 15
+d = a * c / b + a * (a + b)
+print(d)
+print(type(d))
+print(int(d))
+
+for counter in range(10):
+ if (counter % 2) == 0:
+     print(counter)
+     print('is even')
+else:
+ print(counter)
+ print('is odd')
